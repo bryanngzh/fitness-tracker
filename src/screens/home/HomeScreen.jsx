9a33ec215cff9c01889db0b035ff001bcf1d5c76
bryanngzh <bryanngzihao@gmail.com>
2024-07-14 21:19:44 +0800

    feat: add stats to home screen

--- src/screens/home/HomeScreen.jsx
+++ src/screens/home/HomeScreen.jsx
@@ -1,30 +1,57 @@
 import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
 import dayjs from "dayjs";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Box, Button } from "../../components";
+import { apiConfig } from "../../configs/api";
 import { FIREBASE_AUTH } from "../../configs/firebase";
 import styles from "./HomeScreen.style";
 
 const HomeScreen = () => {
   const user = FIREBASE_AUTH.currentUser;
+  const [data, setData] = useState({
+    sessions: "0",
+    streak: "0",
+    distance: "0",
+    gym: "0",
+    workouts: [],
+  });
+
+  useEffect(() => {
+    const fetchAccount = async () => {
+      try {
+        const response = await axios.get(
+          `${apiConfig.apiUrl}/workout?email=${user.email}`
+        );
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching user account: ", error);
+      }
+    };
+    fetchAccount();
+  }, []);
 
   const stats = [
     {
       name: "Lifetime Sessions",
+      value: data.sessions,
       postFix: "🚀",
     },
     {
       name: "Streak",
+      value: data.streak,
       postFix: "🔥",
     },
     {
       name: "Distance Ran",
+      value: data.distance,
       postFix: "KM",
     },
     {
       name: "Gym Workouts",
+      value: data.gym,
     },
   ];
 
@@ -48,7 +75,9 @@ const HomeScreen = () => {
         {stats.map((stat) => (
           <Box key={stat.name} style={styles.box}>
             <Text>{stat.name}</Text>
-            <Text>DATA {stat.postFix}</Text>
+            <Text style={styles.title}>
+              {stat.value} {stat.postFix}
+            </Text>
           </Box>
         ))}
         <Text style={styles.title}>Past Workouts</Text>
